d8d254a54ed57a6ca6dfefb3ef2ecaf13cdf5990
Younes <younes.bns@outlook.com>
2023-06-07 22:50:13 -0400

    the counter is working now

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
 let cartCheckOut = document.getElementById(`cart-checkout`);
 let cartPage = document.getElementById(`cart-page`);
 let addItem = document.getElementById(`add-item`);
+let cartNumber = document.getElementById(`cart-number`);
+let count = 0;
 
 cartCheckOut.addEventListener(`click`, function () {
   cartPage.classList.toggle(`active-cart`);
 });
 
-addItem.addEventListener(`click`, function () {
-  let cartNumber = document.getElementById(`cart-number`);
-  let currentNumber = parseInt(cartNumber.textContent);
-  //   let newNumber = currentNumber + 1;
-  //   currentNumber = newNumber;
-  //   console.log(newNumber);
-  //   console.log(currentNumber);
+// addItem.addEventListener(`click`, function () {
+//   let cartNumber = document.getElementById(`cart-number`);
+//   let currentNumber = parseInt(cartNumber.textContent);
+//   //   let newNumber = currentNumber + 1;
+//   //   currentNumber = newNumber;
+//   //   console.log(newNumber);
+//   //   console.log(currentNumber);
 
-  for (let currentNumber = 0; currentNumber >= 0; currentNumber++) {
-    if (currentNumber !== 0) {
-      //   console.log(currentNumber);
-      let currentNumber = parseInt(cartNumber.textContent);
-      currentNumber++;
-    }
-    if (currentNumber === 0) {
-      break;
-    }
-    console.log(currentNumber);
-  }
+//   for (let currentNumber = 0; currentNumber >= 0; currentNumber++) {
+//     if (currentNumber !== 0) {
+//       //   console.log(currentNumber);
+//       let currentNumber = parseInt(cartNumber.textContent);
+//       currentNumber++;
+//     }
+//     if (currentNumber === 0) {
+//       break;
+//     }
+//     console.log(currentNumber);
+//   }
+// });
+
+addItem.addEventListener(`click`, function () {
+  count++;
+  cartNumber.textContent = count;
+  console.log(count);
 });
