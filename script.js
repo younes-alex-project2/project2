147c8b4a3a5f39d39a36168f4732b6eea67d9535
Younes <younes.bns@outlook.com>
2023-06-07 22:06:41 -0400

    we styled the cart page and add the the pop-up page with js '

---
+++ script.js
@@ -0,0 +1,28 @@
+let cartCheckOut = document.getElementById(`cart-checkout`);
+let cartPage = document.getElementById(`cart-page`);
+let addItem = document.getElementById(`add-item`);
+
+cartCheckOut.addEventListener(`click`, function () {
+  cartPage.classList.toggle(`active-cart`);
+});
+
+addItem.addEventListener(`click`, function () {
+  let cartNumber = document.getElementById(`cart-number`);
+  let currentNumber = parseInt(cartNumber.textContent);
+  //   let newNumber = currentNumber + 1;
+  //   currentNumber = newNumber;
+  //   console.log(newNumber);
+  //   console.log(currentNumber);
+
+  for (let currentNumber = 0; currentNumber >= 0; currentNumber++) {
+    if (currentNumber !== 0) {
+      //   console.log(currentNumber);
+      let currentNumber = parseInt(cartNumber.textContent);
+      currentNumber++;
+    }
+    if (currentNumber === 0) {
+      break;
+    }
+    console.log(currentNumber);
+  }
+});
