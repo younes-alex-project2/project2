cc5f2d01cbbdc99abadde0cb60373ee3f3baea78
Younes <younes.bns@outlook.com>
2023-06-08 19:30:57 -0400

    nn

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
+// -----creating variables for the cart icon and the items -----------
 let cartCheckOut = document.getElementById(`cart-checkout`);
 let cartPage = document.getElementById(`cart-page`);
 let addItem = document.getElementById(`add-item`);
 let cartNumber = document.getElementById(`cart-number`);
 let count = 0;
+imgProd1 = document.getElementById(`prod1`);
+imgProd1.src = "./assets/prod-1.jpg";
 
+// making variables for empty cart
+
+const emptyText = document.getElementById(`empty-text`);
+const purchaseBtn = document.getElementById(`purchase-btn`);
+
+// ----------- pop-ip the cart's page -----------
 cartCheckOut.addEventListener(`click`, function () {
   cartPage.classList.toggle(`active-cart`);
 });
 
-// addItem.addEventListener(`click`, function () {
-//   let cartNumber = document.getElementById(`cart-number`);
-//   let currentNumber = parseInt(cartNumber.textContent);
-//   //   let newNumber = currentNumber + 1;
-//   //   currentNumber = newNumber;
-//   //   console.log(newNumber);
-//   //   console.log(currentNumber);
+// ---------------End with cart page pop up -----------------------------
 
-//   for (let currentNumber = 0; currentNumber >= 0; currentNumber++) {
-//     if (currentNumber !== 0) {
-//       //   console.log(currentNumber);
-//       let currentNumber = parseInt(cartNumber.textContent);
-//       currentNumber++;
-//     }
-//     if (currentNumber === 0) {
-//       break;
-//     }
-//     console.log(currentNumber);
-//   }
-// });
+//------- making the counter working in the icon after clicking in the item---------------
 
 addItem.addEventListener(`click`, function () {
   count++;
   cartNumber.textContent = count;
   console.log(count);
+  emptyText.style.display = `none`;
+  purchaseBtn.style.display = `none`;
+
+  //   display image in the cart page
 });
+
+// ------------------End with counter in the icon --------------------------------
